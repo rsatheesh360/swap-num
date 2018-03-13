c6d219f45c310c555cccdbf46e5d1ac3125bf49a
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-13 17:19:06 +0530

    swap number

---
+++ .gitignore/swap.py
@@ -0,0 +1,7 @@
+a =int(input("enter the frst num:"))
+b =int(input("enter the second num:"))
+print("\nBefore swaping a = %d and b = %d" %(a, b))
+a, b = b, a
+print("\nAfter swaping a = %d and b = %d" %(a, b))
+print()
+print()
